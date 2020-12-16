a0a9a5d620de28037eda401ce367b2dbfcbf514e
SebastianN97 <sebbe132132@gmail.com>
2020-12-16 16:49:00 +0100

    Runs factory generator for user. Modifies factory user. Creating model user and modifying. Adding namespace to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount_devise_token_auth_for 'User', at: 'api/v1/auth', skip: [:omniauth_callbacks]
+
+  namespace :api do
+    namespace :v1 do
+    end
+  end
 end
